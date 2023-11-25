c6073c62613672328031ff8fc890fc5a3823d177
Avijot-singh <singhavijot17@gmail.com>
2023-11-25 16:47:02 +1100

    Creation of Card class and reqired fields and constructor

---
+++ Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardDemo
+{
+    public class Card
+    {
+        private string rank; // This is a field, which gives the abiity to the card calss to store and share the information
+        private string suit;
+
+        public Card() // Constructor is used to initalize the fields
+        {
+        
+        }
+
+    }
+}
